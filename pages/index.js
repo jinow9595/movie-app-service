ae2b5a694e7279c15301fe04e5d8fad0ba2b40a6
jinow9595 <jinow9595@gmail.com>
2022-03-30 15:56:28 +0900

    init setting

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import NavBar from '../components/NavBar'
+
+export default function Home() {
+  return (
+    <>
+      <NavBar />
+      <span>Home</span>
+    </>
+  )
+}
